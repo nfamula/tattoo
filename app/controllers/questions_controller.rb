379755904bba2cd790987cfbac1ae9a8d1fadc32
nfamula <nfamula@gmail.com>
2016-04-05 23:38:09 +0000

    psuedo code

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,11 @@
 class QuestionsController < ApplicationController
+    def index
+    ##create a series of if statements##
+
+    ##have a series of partials which render depending on the answer##
+
+    ##have more if statements to determine the answer##
+
+    ##have a series of partials to render the answer##
+    end
 end
