c85d6183a6dbf28fc3077e251940a62f97069806
nfamula <nfamula@gmail.com>
2016-04-06 19:54:56 +0000

    questions layout

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,12 @@
 class QuestionsController < ApplicationController
     def index
+
     ##create a series of if statements##
 
-    ##have a series of partials which render depending on the answer##
+    ##have a series of partials which render the next question depending on the answer##
 
-    ##have more if statements to determine the answer##
-
-    ##have a series of partials to render the answer##
+    ##have a series of answers which are partials## 
+    ##one of which renders when all of the answers are complete##
+    
     end
 end
